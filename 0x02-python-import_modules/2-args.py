95eb306302e8b3d7b6f2b0f7e22a667ebea46cd2
brianmuthama <brianmuthama96@gmail.com>
2023-05-16 16:03:15 +0300

    python-hello_world

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    total_sum = 0
+    arguments = sys.argv[1:] 
+
+    if len(arguments) == 0:
+        print("0 arguments.")
+    elif len(arguments) == 1:
+        print("1 argument:")
+        print("1:", arguments[0])
+    else:
+        print(f"{len(arguments)} arguments:")
+        for index, argument in enumerate(arguments, start=1):
+            print(f"{index}: {argument}")
